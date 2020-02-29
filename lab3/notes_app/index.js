f2c23f1a2198ab7b8934fe72c329f45f3593236f
tikoStudio <timothy.koenig@mail.com>
2020-02-29 15:27:33 +0100

    finished other methods in js

--- lab3/notes_app/index.js
+++ lab3/notes_app/index.js
@@ -6,11 +6,14 @@ class Note {
     }
     
     createElement(title){
-      let newNote = document.createElement('div');
+      let newNote = document.createElement("div");
       
       // HINT🤩 a.addEventListener('click', this.remove.bind(newNote));
       newNote.innerHTML = `<p>${title}</p> <a href="#" class="card-remove">Remove</a>`;
-      a.addEventListener(`click`, this.remove.bind(newNote));
+      newNote.classList = "card";
+
+      let removeLink = newNote.querySelector(".card-remove");
+      removeLink.addEventListener("click", this.remove.bind(newNote));
 
       return newNote;
     }
@@ -19,23 +22,35 @@ class Note {
       // HINT🤩
       // this function should append the note to the screen somehow
       let completeNote = this.element;
-      document.querySelector(`.notes`).append(completeNote);
+      document.querySelector(".notes").append(completeNote);
     }
     
     saveToStorage(){
       // HINT🤩
       // localStorage only supports strings, not arrays
       // if you want to store arrays, look at JSON.parse and JSON.stringify
-
-      localStorage.setItem(`noteData`, JSON.stringify(Note));
-      JSON.parse(localStorage.getItem(`noteData`));
-      /* incomplete */
+      let localData = JSON.parse(localStorage.getItem("localData"));
+      if(localData != null) {
+        localData.push(this.title);
+        console.log(localData);
+        localStorage.setItem("localData", JSON.stringify(localData));
+      } else {
+        localData = [];
+        localData.push(this.title);
+        console.log(localData);
+        localStorage.setItem("localData", JSON.stringify(localData));
+      }
     }
     
     remove(){
       // HINT🤩 the meaning of 'this' was set by bind() in the createElement function
       // in this function, 'this' will refer to the current note element
-
+      let removeFromArray = JSON.parse(localStorage.getItem("localData"));
+      let getArrayIndex = removeFromArray.indexOf(this); //outputs -1
+      removeFromArray.splice(getArrayIndex, 1);
+      localStorage.setItem("localData", JSON.stringify(removeFromArray));
+      console.log(removeFromArray);
+      this.remove();
     } 
   }
   
@@ -49,13 +64,14 @@ class Note {
       // this.btnAdd = ???
       // this.btnAdd.addEventListener("click", this.createNote.bind(this));
       // this.loadNotesFromStorage();
-      this.btnAdd = document.querySelector(`#btnAddNote`);
-      this.btnAdd.addEventListener(`click`,this.createNote.bind(this));
+      this.btnAdd = document.querySelector("#btnAddNote");
+      this.btnAdd.addEventListener("click",this.createNote.bind(this));
 
-      this.enterAdd = document.querySelector(`#txtAddNote`);
-      this.enterAdd.addEventListener(`keypress`, enter => {
+      this.enterAdd = document.querySelector("#txtAddNote");
+      this.enterAdd.addEventListener("keydown", enter => {
         if(enter.keyCode == 13) {
-          this.createNote.bind(this);
+          document.querySelector("#btnAddNote").click();
+          return true;
         }
       });
       this.loadNotesFromStorage();
@@ -65,6 +81,14 @@ class Note {
       // HINT🤩
       // load all notes from storage here and add them to the screen
       // something like note.add() in a loop would be nice
+      let dataStorage = JSON.parse(localStorage.getItem("localData"));
+      if(dataStorage.length > 0) {
+        dataStorage.forEach(title => {
+          let note = new Note(title);
+          note.add();
+        });
+      }
+      
     }
      
     createNote(e){
@@ -75,15 +99,17 @@ class Note {
       // note.saveToStorage();
       // this.reset();
 
-      let addNoteTitle = document.querySelector(`#textAddNote`).value;
+      let addNoteTitle = document.querySelector(`#txtAddNote`).value;
       let note = new Note(addNoteTitle);
 
-      /* incomplete: add to localstorage */
+      note.add();
+      note.saveToStorage();
+      this.reset();
     }
     
     reset(){
       // this function should reset the form 
-      document.querySelector(`#textAddNote`).value = ``;
+      document.querySelector(`#txtAddNote`).value = ``;
     }
     
   }
