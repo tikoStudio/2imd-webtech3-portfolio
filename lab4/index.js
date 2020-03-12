7b9dfd8efef21d96be12630171e660fda09282c8
tikoStudio <timothy.koenig@mail.com>
2020-03-12 15:47:16 +0100

    reduced load on API by using hourly localstorage

--- lab4/index.js
+++ lab4/index.js
@@ -26,16 +26,39 @@ class WeatherData {
     }
 
     getCurrentWeather(lat, lng) {
-        //sources: {@link: https://www.google.com/search?q=darksky+api+get+weather&oq=darksky+api+get+weather&aqs=chrome..69i57j33l3.4786j0j1&sourceid=chrome&ie=UTF-8#kpvalbx=_cDtqXunxBOvjkgXrpovwCg25}
+        let displayWeather = document.querySelector(`.weather`);
+        let displayToday =  document.querySelector(`.later`);
+        let data = JSON.parse(localStorage.getItem('storedData'));
+        let hour = new Date();
+        hour = hour.getHours();
+        console.log(hour);
+        console.log(data[2])
+        if (data[2] > hour){
+            displayWeather.innerHTML = data[0];
+            displayToday.innerHTML = data[1];
+            console.log("data from localstorage");
+        }else {
+             //sources: {@link: https://www.google.com/search?q=darksky+api+get+weather&oq=darksky+api+get+weather&aqs=chrome..69i57j33l3.4786j0j1&sourceid=chrome&ie=UTF-8#kpvalbx=_cDtqXunxBOvjkgXrpovwCg25}
         let url =  `https://cors-anywhere.herokuapp.com/https://api.darksky.net/forecast/${this.apiKey}/${lat},${lng}`;
         fetch(url)
             .then(response =>{
                 return response.json();
             })
             .then(weather => {
-                let displayWeather = document.querySelector(`.weather`);
                 displayWeather.innerHTML = weather.currently.summary;
+                displayToday.innerHTML = weather.hourly.summary;
+
+                //sources: {@link: https://stackoverflow.com/questions/45561610/how-can-i-remove-local-storage-after-one-hour/45561666}
+                let weatherData = [displayWeather.innerHTML,displayToday.innerHTML];
+                let timeStamp = new Date();
+                weatherData.push(timeStamp.getHours() + 1);
+                localStorage.setItem('storedData', JSON.stringify(weatherData));
             });
+            console.log("data from API");
+        }
+        
+
+       
 
     }
 }
